4fac10e95f2a81983eeca6434787c47f906b7dea
LaurenStipp <lstipp@mchsi.com>
2021-04-20 19:32:15 -0500

    ahhhhh

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,34 +1,7 @@
-let state = 0;
-let numbers;
-
-//fonts
-let simsbold;
-let simsboldSC;
-
 function setup() {
-  createCanvas(1792,828);
-
-  simsbold = loadFont("assets/thesimssansbold.otf");
-  simsboldSC = loadFont("assets/thesimssansboldsc.otf");
+  createCanvas(500, 500);
 }
 
 function draw() {
-  background(66,118,183);
-  // textFont(simsbold);
-  // fill(166,227,41);
-  // clear();
-  // switch (state) {
-  //   case 0:
-  //     text("Tap the screen to start randomization", width/2,height/2);
-  //     break;
-  //
-  //   case 1:
-  //     text("")
-  //     break;
-  //
-  // }
-}
 
-// function touchStarted() {
-//   getAudioContext().resume();
-// }
+}
--- randomLegacy/sketch.js
+++ randomLegacy/sketch.js
@@ -1,7 +1,34 @@
+let state = 0;
+let numbers;
+
+//fonts
+let simsbold;
+let simsboldSC;
+
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(1792,828);
+
+  simsbold = loadFont("assets/thesimssansbold.otf");
+  simsboldSC = loadFont("assets/thesimssansboldsc.otf");
 }
 
 function draw() {
-
+  background(66,118,183);
+  // textFont(simsbold);
+  // fill(166,227,41);
+  // clear();
+  // switch (state) {
+  //   case 0:
+  //     text("Tap the screen to start randomization", width/2,height/2);
+  //     break;
+  //
+  //   case 1:
+  //     text("")
+  //     break;
+  //
+  // }
 }
+
+// function touchStarted() {
+//   getAudioContext().resume();
+// }
